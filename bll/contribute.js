92847cff72b4292269319c023404bc89db330d1d
陈日红 <shadowee@qq.com>
2014-07-02 11:17:46 +0800

    增加模块guid。主键id均使用guid

--- bll/contribute.js
+++ bll/contribute.js
@@ -1,8 +1,11 @@
 var dbc = require('../inc/mysql-client').init();
 
 exports.saveContribute = function(strText){
-    var sql = "insert into joke_contribute (id,contribute_text,contribute_datetime,contribute_userid)";
-    sql += " values(2,'"+ strText+"','2013-1-1 1:1:1','1234')";
+
+    var guid = require("guid").create();
+
+    var sql = "insert into joke_contribute (id,contribute_text,contribute_datetime,contribute_user_id)";
+    sql += " values('"+ guid +"','"+ strText+"',now(),'1234')";
   dbc.insert(sql,null,function(err, res){
       console.log(err);
   });
