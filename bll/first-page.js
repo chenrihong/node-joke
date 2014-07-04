f4836475b2af65cca312be93566517e2356fbc50
陈日红 <shadowee@qq.com>
2014-07-04 14:19:05 +0800

    表名大写换成小写

--- bll/first-page.js
+++ bll/first-page.js
@@ -1,7 +1,7 @@
 var dbc = require('../inc/mysql-client').init();
 
 exports.queryContribute = function(callback){
-    var sql = "SELECT * FROM JOKE_CONTRIBUTE WHERE 1 = 1 LIMIT 0 , 30"
+    var sql = "SELECT * FROM joke_contribute WHERE 1 = 1 LIMIT 0 , 30"
 
     dbc.query(sql,null,function(err, res){
         if(err){
