7884060616be55344f40d9ff1e53d860c72c4aae
陈日红 <shadowee@qq.com>
2014-10-28 11:04:02 +0800

    把项目中的 jsdom 模块 更换为 cheerio 模块

--- bll/first-page.js
+++ bll/first-page.js
@@ -1,50 +1,40 @@
-var jsdom = require("jsdom");
 var http = require("http");
 var fs   = require("fs");
+var cheerio = require("cheerio");
 
 function downloadText(doc){
-    jsdom.env(
-        doc,
-        ["http://code.jquery.com/jquery.js"],
-        function (errors, window) {
-            if(errors){
-                console.log(errors);
-                return;
-            }
-            var $ = window.$;
-            $(".content").each(function(idx,obj){
-                fs.writeFile('public/qiushibaike.txt','',function(err){
-                    if (err) throw err;
-                    fs.appendFile('public/qiushibaike.txt',$(obj).text());
-                    fs.appendFile('public/qiushibaike.txt',"\r\n\r\n");
-                });
-            });
+    var $ = cheerio.load(doc);
 
-            $(".thumb img").each(function(idx,obj){
-                var src = obj.src;
-                if(/pic.qiushibaike.com/.test(src)){
+    $(".content").each(function(idx,obj){
+        fs.writeFile('public/qiushibaike.txt','',function(err){
+            if (err) throw err;
+            fs.appendFile('public/qiushibaike.txt',$(obj).text());
+            fs.appendFile('public/qiushibaike.txt',"\r\n\r\n");
+        });
+    });
 
-                    http.get(src, function(res) {
-                        res.setEncoding('binary');//二进制（binary）
-                        var imageData ='';
-                        res.on('data',function(data){//图片加载到内存变量
-                            imageData += data;
-                        }).on('end',function(){//加载完毕保存图片
-                            var imageName = src.substr(src.length-11,11);
-                            fs.writeFile('public/images/'+imageName, imageData, 'binary', function (err) {//以二进制格式保存
-                                if (err) throw err;
-                                console.log('file saved');
-                            });
-                        });
+    $(".thumb").find("img").each(function(idx,obj){
+        var src = $(obj).attr("src");
+        if(/pic.qiushibaike.com/.test(src)){
 
-                    }).on('error', function(e) {
-                        console.log("Got error: " + e.message);
+            http.get(src, function(res) {
+                res.setEncoding('binary');//二进制（binary）
+                var imageData ='';
+                res.on('data',function(data){//图片加载到内存变量
+                    imageData += data;
+                }).on('end',function(){//加载完毕保存图片
+                    var imageName = src.substr(src.length-12,12);
+                    fs.writeFile('public/images/'+imageName, imageData, 'binary', function (err) {//以二进制格式保存
+                        if (err) throw err;
+                        console.log('file saved');
                     });
-                }
-            });
+                });
 
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
         }
-    );
+    });
 }
 
 function catchCore(){
